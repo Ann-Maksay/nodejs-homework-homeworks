6b087ee787ea4429bea153010732a871263f2a51
Ann Maksay <annaorlova.dn@gmail.com>
2021-08-02 20:19:29 +0300

    fix validate

--- validateShemas/validateUpdatedContact.js
+++ validateShemas/validateUpdatedContact.js
@@ -4,7 +4,7 @@ const schema = Joi.object({
   name: Joi.string().min(2),
   email: Joi.string().email(),
   phone: Joi.string(),
-});
+}).min(1);
 
 const validateNewContact = (obj) => {
   const { error } = schema.validate(obj);
